b1f326824adfdf186c6f398a5bbf917dd5f229cc
Finn <rockerman0074@gmail.com>
2022-01-14 16:35:49 +0800

    0.0.1

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+#app
+  b-nav(tabs vertical )
+    b-nav-item.mt-3
+      img(src="../public/img/tomato.png" width="40px")
+    b-nav-item.mt-3
+      img(src="../public/img/misic.png" width="40px")
+    b-nav-item.mt-3
+      img(src="../public/img/chart.png" width="40px")
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+html,body{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#app{
+  height: 100%;
+  width: 100%;
+  background: #FFE5B5;
+  position: relative;
+}
+.nav{
+  position: absolute;
+  top: 20px;
+  right: 40px;
 }
 </style>
